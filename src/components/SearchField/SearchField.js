820ecfdb9b26129b47def7827ccba642f72fcb67
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2018-12-27 09:01:19 +0100

    Validate search field

--- src/components/SearchField/SearchField.js
+++ src/components/SearchField/SearchField.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { connect } from "react-redux";
 
 
 class SearchField extends React.Component {
 
 
     handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
+        if (e.key === 'Enter' && e.target.value.length > 0) {
             this.props.onClick();
         } else {
             this.props.onKeyUp(e);
@@ -14,16 +15,23 @@ class SearchField extends React.Component {
 
     render(){
         const typeSearch = this.props.typeSearch;
+        const isDisabled = this.props.state.dataSearch ? '' : 'search-field__button--disabled';
 
         return(
             <div className="search-field">
                 <div className="search-field__wrap">
                     <input className="search-field__input" placeholder={`Search ${typeSearch}...`} type="text" onKeyUp={(e) => this.handleKeyPress(e) }  />
-                    <button className="search-field__button" onClick={this.props.onClick} >Search</button>
+                    <button className={`search-field__button ${isDisabled}`} onClick={this.props.onClick} >Search</button>
                 </div>
             </div>
         )
     }
 } 
 
-export default SearchField;
+
+const mapStateToProps = state => ({ state });
+
+export default connect(
+	mapStateToProps,
+	null
+  )(SearchField)
--- src/reducer/actions/fetchData.js
+++ src/reducer/actions/fetchData.js
@@ -2,7 +2,7 @@ import APIKEY from '../../apikey';
 import { fetchTempLoading, fetchTempSuccess, fetchTempError } from '../reducer';
 
 export const fetchData = (state, moreResults, page, typeSearch) => dispatch => {
-    
+
     const url = `http://www.omdbapi.com/?apikey=${APIKEY}&s=${state.dataSearch}&page=${page}&type=${typeSearch}` 
 
     dispatch(fetchTempLoading());
