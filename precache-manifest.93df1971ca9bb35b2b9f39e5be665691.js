5c765cd631be5ac9395bf3f7fc9a1613e3d442c0
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2018-12-28 13:26:05 +0100

    Updates

---
+++ precache-manifest.93df1971ca9bb35b2b9f39e5be665691.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08b5c583f1842581bd07",
+    "url": "/movie-finder-react/static/js/runtime~main.08b5c583.js"
+  },
+  {
+    "revision": "ae498be62ab35e3a950b",
+    "url": "/movie-finder-react/static/js/main.ae498be6.chunk.js"
+  },
+  {
+    "revision": "5ff035959c7f2b0c9d8c",
+    "url": "/movie-finder-react/static/js/1.5ff03595.chunk.js"
+  },
+  {
+    "revision": "ae498be62ab35e3a950b",
+    "url": "/movie-finder-react/static/css/main.114e9ced.chunk.css"
+  },
+  {
+    "revision": "91bd22a0d4206bdf7208726a7bac3369",
+    "url": "/movie-finder-react/index.html"
+  }
+];
