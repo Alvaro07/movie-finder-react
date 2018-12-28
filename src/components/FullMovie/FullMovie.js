78933a76854c61256ee9c501dafc3a624631ee85
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2018-12-28 10:53:49 +0100

    Add content and styles

--- src/components/FullMovie/FullMovie.js
+++ src/components/FullMovie/FullMovie.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from "react-redux";
 import Loader from '../Loader/Loader'; 
 import { fetchFullData } from "../../reducer/actions/fetchFullData";
-import { fetchDataLoading } from '../../reducer/reducer';
 
 class FullMovie extends React.Component {
 
@@ -12,13 +11,50 @@ class FullMovie extends React.Component {
     
     render(){
 
+        
+
         if ( !this.props.state.fullContent ) {
             return <Loader />
             
         } else {
+
+            const webSite = this.props.state.fullContent.Website === 'N/A' ? null : this.props.state.fullContent.Website;
+            const poster = this.props.state.fullContent.Poster !== 'N/A'? this.props.state.fullContent.Poster : 'https://images.pexels.com/photos/65128/pexels-photo-65128.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
+
+            console.log(this.props.state.fullContent)
             return(
                 <div className="full-movie">
-                    {this.props.state.fullContent.Title}
+
+                    <div className="full-movie__image">
+                        <img src={poster} alt={this.props.state.fullContent.Title} />
+                    </div>
+
+                    <div className="full-movie__content">
+                        <h2 className="full-movie__title">{this.props.state.fullContent.Title} <span>({this.props.state.fullContent.Year})</span></h2>
+
+
+                        <ul className="full-movie__entry-list">
+                            <li>{this.props.state.fullContent.Runtime}</li>
+                            <li>{this.props.state.fullContent.Released}, {this.props.state.fullContent.Country}</li>
+                            <li>{this.props.state.fullContent.Genre}</li>
+                        </ul>
+
+                        <p className="full-movie__description">{this.props.state.fullContent.Plot}</p>
+                        
+                        <p className="full-movie__sub"><span className="bold">Director: </span> {this.props.state.fullContent.Director}</p>
+                        <p className="full-movie__sub"><span className="bold">Writer: </span> {this.props.state.fullContent.Writer}</p>
+                        <p className="full-movie__sub"><span className="bold">Actors: </span> {this.props.state.fullContent.Actors}</p>
+                        <p className="full-movie__sub"><span className="bold">Production: </span> {this.props.state.fullContent.Production}</p>
+                        <p className="full-movie__sub"><span className="bold">Awards: </span> {this.props.state.fullContent.Awards}</p>
+                        
+                        
+                        { webSite &&
+                            <p className="full-movie__website">
+                                <a href={this.props.state.fullContent.Website} target="_blank">{this.props.state.fullContent.Website}</a>
+                            </p>
+                        }
+                        
+                    </div>
                 </div>
             )
         }
@@ -30,8 +66,7 @@ class FullMovie extends React.Component {
 
 const mapStateToProps = state => ({ state });
 const mapDispatchToProps = (dispatch) => ({
-    getFullData: (id) => dispatch( fetchFullData(id) ),
-    activeLoader: () => dispatch ( fetchDataLoading() )
+    getFullData: (id) => dispatch( fetchFullData(id) )
 });
 
 export default connect(
