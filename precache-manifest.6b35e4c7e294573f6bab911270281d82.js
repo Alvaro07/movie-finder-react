4c6f33824aeeceaee51d903e0284270cc5bdfb94
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2018-12-31 10:41:30 +0100

    Updates

--- precache-manifest.6b35e4c7e294573f6bab911270281d82.js
+++ precache-manifest.6b35e4c7e294573f6bab911270281d82.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/movie-finder-react/static/js/runtime~main.08b5c583.js"
   },
   {
-    "revision": "485823602a7c36013391",
-    "url": "/movie-finder-react/static/js/main.48582360.chunk.js"
+    "revision": "ae0224d1e7414da35e9f",
+    "url": "/movie-finder-react/static/js/main.ae0224d1.chunk.js"
   },
   {
     "revision": "5ff035959c7f2b0c9d8c",
     "url": "/movie-finder-react/static/js/1.5ff03595.chunk.js"
   },
   {
-    "revision": "485823602a7c36013391",
+    "revision": "ae0224d1e7414da35e9f",
     "url": "/movie-finder-react/static/css/main.114e9ced.chunk.css"
   },
   {
-    "revision": "8313fbc5284e3310fabadac600a8d9e0",
+    "revision": "a84a4ce829b439e3b49f1339cad1713a",
     "url": "/movie-finder-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-finder-react/precache-manifest.4bd85ef4d9562b31ca7f3956d63bbb34.js"
+  "/movie-finder-react/precache-manifest.6b35e4c7e294573f6bab911270281d82.js"
 );
 
 workbox.clientsClaim();
